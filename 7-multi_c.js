736f7c704e783e6ae390fc10fbb4216daa155479
reitumetsetsephe <ST10433587@rcconnect.edu.za>
2025-07-13 15:38:34 +0200

    Fix handling of negative and missing arguments in 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,12 @@
 const x = parseInt(process.argv[2]);
-if (!x || x < 1) {
+
+if (!process.argv[2] || isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
+} else if (x > 0) {
   let i = 0;
   while (i < x) {
     console.log("C is fun");
     i++;
   }
 }
+// If x is zero or negative, print nothing
